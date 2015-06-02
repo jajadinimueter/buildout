d8ec1f2c4a33b7244d9972863ac62dbaef433cca
Florian Mueller <florian.mueller@hostpoint.ch>
2015-06-02 14:03:12 +0200

    Changed version to local one

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 #
 ##############################################################################
 name = "zc.buildout"
-version = "2.3.1"
+version = "2.3.1+hostpoint.1"
 
 import os
 from setuptools import setup
